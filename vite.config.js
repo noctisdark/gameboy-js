5fd0ee51b6a130942f046cc8233ee46bbb74187d
noctisdark <diseven@outlook.fr>
2023-05-13 20:38:18 +0200

    vite live reload

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,12 @@
+import liveReload from 'vite-plugin-live-reload'
+
 module.exports = {
   root: "./",
   server: {
     port: 3000,
   },
+
+  plugins: [
+    liveReload('./**/*.{js,html,css}'),
+  ]
 };
